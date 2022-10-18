7e376f0f6439eaf8cf280e1181613746653d481f
LVN <108108233+LvnDev@users.noreply.github.com>
2022-10-18 19:50:41 +0100

    Add project files.

---
+++ Plane_Tickets/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Plane_Tickets.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style>
+        body {background-color:beige;}
+        h1,h2,h3,h4,h5,p,label,button{color:black;font-family:Arial;}
+    </style>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p></p>
+<h2>Air Euro  <asp:Image ID="Logo" runat="server" Height="50px" ImageUrl="~/Images/Logo.png" Width="60px" />
+        </h2>
+<h4>From:</h4>
+        <asp:RadioButton ID="fromLondon" runat="server" GroupName="fromRadioButton" Text="London" /> 
+        <asp:RadioButton ID="fromParis" runat="server" Text="Paris" GroupName="fromRadioButton" />
+        <asp:RadioButton ID="fromRiga" runat="server" Text="Riga" GroupName="fromRadioButton" />
+        <asp:RadioButton ID="fromMadrid" runat="server" Text="Madrid" GroupName="fromRadioButton" />
+<h4>To:</h4>
+        <asp:RadioButton ID="toLondon" runat="server" Text="To London" GroupName="toRadioButton"/>
+        <asp:RadioButton ID="toParis" runat="server" Text="To Paris" GroupName="toRadioButton"/>
+        <asp:RadioButton ID="toRiga" runat="server" Text="To Riga" GroupName="toRadioButton" />
+        <asp:RadioButton ID="toMadrid" runat="server" Text="To Madrid" GroupName="toRadioButton" />
+        <br />
+        <br />
+        <asp:CheckBox ID="returnCheckBox" runat="server" Text="Add Return Flight?" />
+        <br />
+        <br />
+        <asp:Button ID="Submit" runat="server" Text="Submit" OnClick="Submit_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Plane_Tickets/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Plane_Tickets
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        double[,] priceGrid;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            string[] cities = new string[4]; //4 arrays of cities
+            cities[0] = "London"; //london
+            cities[1] = "Paris"; // paris
+            cities[2] = "Riga"; // Riga
+            cities[3] = "Madrid"; // Madrid
+
+            priceGrid = new double[4, 4];
+            //0 London
+            //1 Paris
+            //2 Riga
+            //3 Madrid
+            priceGrid[0, 1] = 35.0; //London
+            priceGrid[0, 2] = 45.99;
+            priceGrid[0, 3] = 49.99;
+
+            priceGrid[1, 0] = 35.0; //Paris
+            priceGrid[1, 2] = 40.0;
+            priceGrid[1, 3] = 44.99;
+
+            priceGrid[2, 0] = 45.99; //Riga
+            priceGrid[2, 1] = 40.0;
+            priceGrid[2, 3] = 55.0;
+
+            priceGrid[3, 0] = 49.99; //Madrid
+            priceGrid[3, 1] = 44.99;
+            priceGrid[3, 2] = 55.0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    resultLabel.Text += cities[i] + " To " + cities[j] + " £" +  priceGrid[i, j].ToString() + "<br>" + "<br>";
+                }
+            }    
+        }
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            int fromCity;
+            if (fromLondon.Checked) fromCity = 0;
+            else if (fromParis.Checked) fromCity = 1;
+            else if (fromRiga.Checked) fromCity = 2;
+            else fromCity = 3;
+
+            int toCity;
+            if (toLondon.Checked) toCity = 0;
+            else if (toParis.Checked) toCity = 1;
+            else if (toRiga.Checked) toCity = 2;
+            else toCity = 3;
+
+            resultLabel.Text = " £" + priceGrid[fromCity, toCity].ToString();
+
+            if (returnCheckBox.Checked)
+            {
+                double times2 = priceGrid[toCity, fromCity] + priceGrid[toCity, fromCity];
+                resultLabel.Text = " £" + times2.ToString();
+            }
+
+            if (fromCity == toCity)
+            {
+                resultLabel.Text = "ERROR! Please Select 2 cities thar are not the same.";
+                return;
+            }
+
+        }
+    }
+}
